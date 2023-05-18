4c99858a0928fb28890fc67002e282e11f08b054
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-18 14:07:03 +0000

    Changes for the pages test

--- troubadour/cli.py
+++ troubadour/cli.py
@@ -5,3 +5,14 @@ import click
 @click.option("-f", "--flag", is_flag=True)
 def main(flag: bool):
     print(f"hello {flag}")
+    f = open("index.html", "w")
+    f.write("""<html>
+<header></header>
+
+<body>
+    <h1>Hello</h1>
+    testing GH pages w/ actions!
+</body>
+
+</html>""")
+    f.close()
