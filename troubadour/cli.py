50154b3fb90d804a53fe8ec6b4987fb1ec07d6e9
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-19 12:48:41 +0000

    First jinja template

--- troubadour/cli.py
+++ troubadour/cli.py
@@ -1,18 +1,13 @@
 import click
+from jinja2 import Environment, PackageLoader
 
 
 @click.command()
 @click.option("-f", "--flag", is_flag=True)
 def main(flag: bool):
-    print(f"hello {flag}")
+    environment = Environment(loader=PackageLoader("troubadour"))
+    main_template = environment.get_template("main.html.j2")
+
     f = open("index.html", "w")
-    f.write("""<html>
-<header></header>
-
-<body>
-    <h1>Hello</h1>
-    testing GH pages w/ actions!
-</body>
-
-</html>""")
+    f.write(main_template.render(message="Hello world!!!"))
     f.close()
