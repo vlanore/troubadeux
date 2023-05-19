bf0db39f66624e48d0bb034ee09415f718d269be
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-19 15:48:51 +0000

    Fix: missing mkdir

--- troubadour/cli.py
+++ troubadour/cli.py
@@ -10,6 +10,7 @@ def main(path: str) -> None:
     environment = Environment(loader=PackageLoader("troubadour"))
     main_template = environment.get_template("main.html.j2")
 
+    Path(path).mkdir(parents=True, exist_ok=True)
     main_path = Path(path) / "index.html"
     main_source = main_template.render(message="Hello world!!!")
     main_path.write_text(main_source)
