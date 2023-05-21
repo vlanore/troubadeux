d0deab0e51e8ea522a4d020533515882492d58c9
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-21 22:16:08 +0000

    Can now write to body

--- troubadour/backend.py
+++ troubadour/backend.py
@@ -1,6 +1,11 @@
-import pyscript
+import pyscript  # type:ignore
+import js
 
 
 def pyscript_version() -> str:
     "Returns the version of pyscript currently running."
     return pyscript.__version__
+
+
+def add_to_body(text: str) -> None:
+    pyscript.Element("body").write(text, append=True)
--- troubadour/cli.py
+++ troubadour/cli.py
@@ -43,7 +43,6 @@ def build(path: str, entry_point: str, src: str) -> None:
 
     # Copy troubadour lib to dest folder
     troubadour_dest_dir = output_path / "troubadour"
-    troubadour_dest_dir.mkdir(parents=True)
     troubadour_spec = find_spec("troubadour")
     assert troubadour_spec is not None and troubadour_spec.origin is not None
     troubadour_module_dir = Path(troubadour_spec.origin).parent
@@ -52,6 +51,10 @@ def build(path: str, entry_point: str, src: str) -> None:
     troubadour_filtered_files = [
         f for f in troubadour_files if str(f)[:5] != "__pyc" and "cli.py" not in str(f)
     ]
+    if troubadour_dest_dir.exists():
+        shutil.rmtree(troubadour_dest_dir)
+    troubadour_dest_dir.mkdir(parents=True)
+
     for f in troubadour_filtered_files:
         shutil.copyfile(f, troubadour_dest_dir / f.relative_to(troubadour_module_dir))
     troubadour_files_to_fetch = [
