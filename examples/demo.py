2234e1e0ab7ed2c7e828b6adbe1bf79b4cd9cf15
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-22 15:39:43 +0000

    Tested local storage in demo

--- examples/demo.py
+++ examples/demo.py
@@ -2,4 +2,9 @@ import troubadour.backend as be
 
 print(f"Advanced demo, running pyscript {be.pyscript_version()}")
 be.insert_end("body", "<h1>Hello</h1>\n")
-be.insert_end("body", "world!!a")
+be.insert_end("body", f"world!! {be.local_storage.has_key('hello')}")
+if be.local_storage.has_key("hello"):
+    hello = be.local_storage(int)["hello"]
+    assert hello is not None
+    be.insert_end("body", f"<div>Hello: <b>{hello}</b></div>")
+    be.local_storage["hello"] = hello + 1
