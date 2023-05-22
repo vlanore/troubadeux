2d4092427d0ec1c1001e94efd9dddf488fc3570f
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-22 15:49:21 +0000

    Added test button to demo

--- examples/demo.py
+++ examples/demo.py
@@ -5,6 +5,13 @@ be.insert_end("body", "<h1>Hello</h1>\n")
 be.insert_end("body", f"world!! {be.local_storage.has_key('hello')}")
 if be.local_storage.has_key("hello"):
     hello = be.local_storage(int)["hello"]
-    assert hello is not None
     be.insert_end("body", f"<div>Hello: <b>{hello}</b></div>")
     be.local_storage["hello"] = hello + 1
+
+
+def f(_) -> None:
+    be.insert_end("body", "<div>hello</div>")
+
+
+be.insert_end("body", "<button type='button' id='but'>Click!</button>")
+be.onclick("but", f)
