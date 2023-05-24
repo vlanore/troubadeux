2e9874b0dc0dbfbb41660e19d503f79c6c04e65f
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-24 16:39:22 +0000

    Basic passage demo

--- troubadour/run.py
+++ troubadour/run.py
@@ -1,6 +1,8 @@
+import troubadour.backend as be
 import troubadour.definitions as df
 
 
 def run(passage: df.Passage, context: df.Context) -> None:
+    be.clear("input")
     continuation = passage(context)
     continuation.setup(df.ElementId("input"), context)
