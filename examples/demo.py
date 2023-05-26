bd7cd13815a677a9aa6c320c792104864953527b
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-26 10:35:03 +0000

    Moved continuations to separate file

--- examples/demo.py
+++ examples/demo.py
@@ -1,25 +1,13 @@
-from dataclasses import dataclass
 import troubadour.backend as be
 import troubadour.definitions as df
 from troubadour.definitions import Context, ElementId
 from troubadour.run import run
-from troubadour.unique_id import get_unique_element_id
-
-
-@dataclass
-class Button(df.Continuation):
-    txt: str
-    passage: df.Passage
-
-    def setup(self, target: ElementId, state: df.State = df.empty_state) -> None:
-        id = get_unique_element_id("button")
-        be.insert_end(target, f"<button type='button' id='{id}'>{self.txt}</button>")
-        be.onclick(id, lambda _: run(self.passage, Context()))
+from troubadour.continuations import Button, ContinuationList
 
 
 def my_passage(context: Context) -> df.Continuation:
     print("Hello")
-    return df.ContinuationList(
+    return ContinuationList(
         Button("Click", my_other_passage), Button("Clack", my_passage)
     )
 
