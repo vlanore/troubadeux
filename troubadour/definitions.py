d10be6a8d803e08a70734159078be31f7c5fab17
Vincent Lanore <vlanore@users.noreply.github.com>
2023-05-26 10:23:21 +0000

    Some typing for state

--- troubadour/definitions.py
+++ troubadour/definitions.py
@@ -5,16 +5,23 @@ ElementId = NewType("ElementId", str)
 "Id of an element on the page (typically, a HTML tag)."
 
 
+class State:
+    ...
+
+
+empty_state = State()
+
+
 class Continuation(Protocol):
     "Inputs are user interface elements"
 
-    def setup(self, target: ElementId, context: "Context") -> None:
+    def setup(self, target: ElementId, state: State = empty_state) -> None:
         ...
 
 
 @dataclass
 class Context:
-    state: Any
+    state: State = empty_state
     args: tuple = ()
     kwargs: dict[str, Any] = field(default_factory=dict)
 
