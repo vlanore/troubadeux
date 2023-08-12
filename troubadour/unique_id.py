67631d2f5fb49dc15ba7d51f7ce7a6ea9d605207
Vincent Lanore <vlanore@users.noreply.github.com>
2023-08-12 12:26:44 +0000

    Added documentation to the unique_id file

--- troubadour/unique_id.py
+++ troubadour/unique_id.py
@@ -4,23 +4,32 @@ from troubadour.definitions import ElementId
 
 
 @dataclass
-class IdProvider:
+class _IdProvider:
+    "Class that provides unique ints on demand through its `get` method."
     next_id: int = 0
 
     def get(self) -> int:
+        "Returns a unique int every time."
         result = self.next_id
         self.next_id += 1
         return result
 
 
-_global_provider = IdProvider()
+_global_provider = _IdProvider()
+"Global id provider."
 
 
 def get_unique_id() -> int:
+    "Returns a unique int every time."
     global _global_provider
     return _global_provider.get()
 
 
 def get_unique_element_id(name: str = "") -> ElementId:
+    """Returns a unique troubadour string identifier of the form "troubadour_XY"
+    where X is the optional `name` argument, and Y is a unique integer.
+
+    Args:
+        - name (`str`): optional name for identifier sub-category."""
     to_insert = name + "__" if name != "" else ""
     return ElementId(f"troubadour__{to_insert}{get_unique_id()}")
