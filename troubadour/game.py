3367f731c5dbb14fa5d4dd3b51a5cfbacc8537bb
Vincent Lanore <vlanore@users.noreply.github.com>
2023-08-16 13:07:23 +0000

    Refactor: separate ResetDialog class

--- troubadour/game.py
+++ troubadour/game.py
@@ -19,6 +19,31 @@ class Interface(Protocol):
         ...
 
 
+class ResetDialog:
+    @classmethod
+    def perform_reset(cls, game: "Game") -> None:
+        sv.erase_save()
+        be.refresh_page()
+
+    @classmethod
+    def dialog(cls, game: "Game") -> Interface:
+        be.insert_end(
+            eid("output"),
+            ("<h1>Game reset</h1>"
+             "<p>Are you sure you want to reset?</p>"
+             "<p>This will <b>erase all game data</b>."),
+        )
+        return tc.InterfaceSequence(
+            tc.Button("Reset", cls.perform_reset, dialog=True),
+            tc.Button("Cancel", Game.cancel_dialog, dialog=True),
+        )
+
+    @classmethod
+    def callback(cls, _) -> None:
+        game = sv.load_game()
+        game.run_passage(cls.dialog, dialog=True)
+
+
 @dataclass
 class Game(Generic[T]):
     state: T
@@ -35,31 +60,6 @@ class Game(Generic[T]):
         t = ts.strftime(r"%Y - %b %d - %H:%M:%S")
         be.insert_end(eid("output"), f"<div class='timestamp'>{t}</div>")
 
-    @classmethod
-    def perform_reset(cls, game: "Game") -> None:
-        sv.erase_save()
-        be.refresh_page()
-
-    @classmethod
-    def cancel_dialog(cls, game: "Game") -> None:
-        game.render()
-
-    @classmethod
-    def reset_dialog(cls, game: "Game") -> Interface:
-        be.insert_end(
-            eid("output"),
-            ("<h1>Game reset</h1>" "<p>Are you sure you want to reset?</p>"),
-        )
-        return tc.InterfaceSequence(
-            tc.Button("Reset", cls.perform_reset, dialog=True),
-            tc.Button("Cancel", cls.cancel_dialog, dialog=True),
-        )
-
-    @classmethod
-    def reset_callback(cls, _) -> None:
-        game = sv.load_game()
-        game.run_passage(cls.reset_dialog, dialog=True)
-
     def render(self) -> None:
         be.clear(eid("output"))
         be.clear(eid("input"))
@@ -73,6 +73,10 @@ class Game(Generic[T]):
         if self.input_state is not None:
             self.input_state.setup(self)
         be.scroll_to_bottom(eid("output"))
+
+    @classmethod
+    def cancel_dialog(cls, game: "Game") -> None:
+        game.render()
 
     @classmethod
     def run(cls, StateCls: type, start_passage: Callable) -> None:
@@ -89,7 +93,7 @@ class Game(Generic[T]):
         be.file_download_button(eid("export"), game_json, "troubadour.json")
 
         # reset button
-        be.onclick(eid("reset"), cls.reset_callback)
+        be.onclick(eid("reset"), ResetDialog.callback)
 
     def run_passage(
         self, passage: Callable, *, dialog: bool = False, kwargs: dict[str, object] = {}
